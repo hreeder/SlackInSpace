0b4b18923a44fa1651a8cc732a150f3a8b2861a7
Harry Reeder <harry@harryreeder.co.uk>
2015-04-12 12:59:49 +0100

    Add name to Slack Team Model

--- slackeden/models/slack.py
+++ slackeden/models/slack.py
@@ -3,6 +3,7 @@ from slackeden import db
 
 class Team(db.Model):
     id = db.Column(db.String(64), primary_key=True)
+    name = db.Column(db.String(64))
     subdomain = db.Column(db.String(64))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     token = db.Column(db.String(64))
